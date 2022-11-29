258b599e2cd8b5df118cf944760a95d8c81a6c84
Murat <muratmirsad@gmail.com>
2022-11-29 16:24:02 +0300

    Update ft_putunsigned.c

--- 02 Printf/ft_putunsigned.c
+++ 02 Printf/ft_putunsigned.c
@@ -5,8 +5,8 @@ void	ft_putunsigned(unsigned int nb, int i)
 	(void)i;
 	if (nb > 9)
 	{
-		ft_putnbr(nb / 10, i);
-		ft_putnbr(nb % 10, i);
+		ft_putunsigned(nb / 10, i);
+		ft_putunsigned(nb % 10, i);
 	}
 	else
 	{
