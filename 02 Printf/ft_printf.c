6270869f9da336ad3a7c42a428811b6adf0d914a
Mirsad <muratmirsad@gmail.com>
2022-12-01 00:08:20 +0300

    01/12

--- 02 Printf/ft_printf.c
+++ 02 Printf/ft_printf.c
@@ -33,6 +33,8 @@ int	ft_printf(const char *str, ...)
 	i = 0;
 	while (*(str + i))
 	{
+		while (str[i] == '%' && str[i + 1] == '%')
+			ft_putchar(str[i++], 0);
 		if (str[i] == '%' && (str[i + 1] == 'd' || str[i + 1] == 'i'))
 			ft_putnbr(va_arg(ar, int), i++);
 		else if (str[i] == '%' && str[i + 1] == 's')
@@ -40,8 +42,8 @@ int	ft_printf(const char *str, ...)
 		else if (str[i] == '%' && str[i + 1] == 'u')
 			ft_putunsigned(va_arg(ar, unsigned int), i++);
 		else if (str[i] == '%' && (str[i + 1] == 'x' || str[i + 1] == 'X'))
-			ft_puthexadecimal(va_arg(ar, unsigned int), (int)&str[++i]);
-		else
+			ft_puthexadecimal(va_arg(ar, unsigned int), (int)str[++i]);
+		else if (!(str[i] == '%'))
 			ft_putchar(str[i], i);
 		i++;
 	}
--- 02 Printf/main.c
+++ 02 Printf/main.c
@@ -9,8 +9,13 @@ int main()
 	//printf("\nu '87263323':%u\nu '-87263323':%u\nd '-87263323':%d\n", 87263323, -87263323, -87263323);
 
 
-	ft_printf("x: %x\nx: %x\nd: %d\nd: %d\nu: %u\nu: %u\n", 123, -123, 123, -123, 123, -123);
-	printf("\nORGINAL\nx: %x\nx: %x\nd: %d\nd: %d\nu: %u\nu: %u\n", 123, -123, 123, -123, 123, -123);
+	ft_printf("FT_PRINTF\nx: %x\nx: %x\nd: %d\nd: %d\nu: %u\nu: %u\n", 123, -123, 123, -123, 123, -123);
+	printf("\nORGINAL\nX: %X\nx: %x\nx: %x\nd: %d\nd: %d\nu: %u\nu: %u\n",123, 123, -123, 123, -123, 123, -123);
+
+	ft_printf("\n\n------------------\n\n");
+
+	ft_printf("FT_PRINTF\n%%%");
+	printf("ORGINAL\n%%%\n");
 
 	return (0);
 }
