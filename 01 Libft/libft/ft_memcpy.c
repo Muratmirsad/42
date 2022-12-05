18caa7bc31d25fa35970118d738033ff8d603e1d
Muratmirsad <muratmirsad@gmail.com>
2022-12-05 17:18:38 +0300

    02/12

--- 01 Libft/libft/ft_memcpy.c
+++ 01 Libft/libft/ft_memcpy.c
@@ -6,25 +6,23 @@
 /*   By: mdiraga <mdiraga@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 10:28:15 by mdiraga           #+#    #+#             */
-/*   Updated: 2022/12/05 12:10:57 by mdiraga          ###   ########.fr       */
+/*   Updated: 2022/12/05 17:08:23 by mdiraga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
-	if (!dest && !src)
+	if (!dst && !src)
 		return (0);
-	if ((dest == src) || !n)
-		return (dest);
 	i = 0;
 	while (i < n)
 	{
-		*((char *)dest + i) = *((char *)src + i);
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
-	return (dest);
+	return (dst);
 }
