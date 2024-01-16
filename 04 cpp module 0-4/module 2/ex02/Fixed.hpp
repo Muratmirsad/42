da3c030467e4d62ed1447c5999ef04bd5a735ef5
Murat Mirsad Dırağa <mdiraga@Murats-Mac-mini.local>
2024-01-16 17:45:19 +0300

    readme

--- 04 cpp module 0-4/module 2/ex02/Fixed.hpp
+++ 04 cpp module 0-4/module 2/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: mdiraga <mdiraga@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 14:49:10 by mdiraga           #+#    #+#             */
-/*   Updated: 2024/01/16 15:13:34 by mdiraga          ###   ########.fr       */
+/*   Updated: 2024/01/16 15:44:08 by mdiraga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,34 +22,34 @@ private:
     static const int  bits = 8;
 
 public:
-                Fixed();
-                Fixed( const Fixed &cpy );
-                Fixed( const int value );
-                Fixed( const float float_value );
-                ~Fixed();
-    Fixed&      operator=( const Fixed &cpy );
-    int         getRawBits( void );
-    void        setRawBits( int const raw );
-    int		    toInt( void ) const;
-    float	    toFloat( void ) const;
-    bool	    operator==( const Fixed &fixed );
-    bool	    operator!=( const Fixed &fixed );
-    Fixed	    operator+( const Fixed &fixed );
-    Fixed       operator-( const Fixed &fixed );
-    bool	    operator<( const Fixed &fixed );
-    bool	    operator>( const Fixed &fixed );
-    bool        operator<=( const Fixed &fixed );
-    bool	    operator>=( const Fixed &fixed );
-    Fixed	    operator*( const Fixed &fixed );
-    Fixed       operator/( const Fixed &fixed );
-    Fixed&      operator++();
-    Fixed       operator++( int );
-    Fixed&      operator--();
-    Fixed       operator--( int );
-    static const Fixed &max( const Fixed &a, const Fixed &b );
-    static const Fixed &min( const Fixed &a, const Fixed &b );
-    static Fixed       &max( Fixed &a, Fixed &b );
-    static Fixed       &min( Fixed &a, Fixed &b );
+                        Fixed();
+                        Fixed( const Fixed &cpy );
+                        Fixed( const int value );
+                        Fixed( const float float_value );
+                        ~Fixed();
+    Fixed&              operator=( const Fixed &cpy );
+    int                 getRawBits( void );
+    void                setRawBits( int const raw );
+    int		            toInt( void ) const;
+    float	            toFloat( void ) const;
+    bool	            operator==( const Fixed &fixed );
+    bool	            operator!=( const Fixed &fixed );
+    Fixed	            operator+( const Fixed &fixed );
+    Fixed               operator-( const Fixed &fixed );
+    bool	            operator<( const Fixed &fixed );
+    bool	            operator>( const Fixed &fixed );
+    bool                operator<=( const Fixed &fixed );
+    bool	            operator>=( const Fixed &fixed );
+    Fixed	            operator*( const Fixed &fixed );
+    Fixed               operator/( const Fixed &fixed );
+    Fixed&              operator++();
+    Fixed               operator++( int );
+    Fixed&              operator--();
+    Fixed               operator--( int );
+    static const Fixed  &max( const Fixed &a, const Fixed &b );
+    static const Fixed  &min( const Fixed &a, const Fixed &b );
+    static Fixed        &max( Fixed &a, Fixed &b );
+    static Fixed        &min( Fixed &a, Fixed &b );
 };
 
 std::ostream&	operator<<(std::ostream& os, const Fixed &fixed);
