1c776073422e9f7ab913be66aae606e0e9df778f
Murat Mirsad Dırağa <mdiraga@Murats-Mac-mini.local>
2024-04-27 16:33:44 +0300

    cpp5 ex01

---
+++ 05 cpp module 5-9/module 5/ex01/Bureaucrat.hpp
@@ -0,0 +1,43 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include "Form.hpp"
+
+class Bureaucrat {
+	private:
+		const std::string	name;
+		int					grade;
+
+	public:
+		class GradeTooHighException : public std::exception {
+		public:
+			virtual const char* what() const throw();
+			GradeTooHighException() throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+		public:
+			virtual const char* what() const throw();
+			GradeTooLowException() throw();
+		};
+
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& other);
+		~Bureaucrat();
+
+		Bureaucrat&	operator=(const Bureaucrat& other);
+
+		std::string	getName() const;
+		int			getGrade() const;
+
+		void		incrementGrade();
+		void		decrementGrade();
+		void		signForm(class Form& form);
+
+		friend std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
+};
+
+#endif
---
+++ 05 cpp module 5-9/module 5/ex01/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int main() {
+    try
+    {
+        Bureaucrat alice("Alice", 50);
+        Bureaucrat bob("Bob", 25);
+
+        Form taxForm("Tax Form", 30, 40);
+
+        std::cout << "Before signing:\n" << taxForm << std::endl;
+
+        alice.signForm(taxForm);
+        std::cout << "After Alice's attempt to sign:\n" << taxForm << std::endl;
+
+        bob.signForm(taxForm);
+        std::cout << "After Bob's attempt to sign:\n" << taxForm << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
